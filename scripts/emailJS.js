7c4b07454912bbd64e37c498b342335a72335bbf
t0miXhi-II <tomishittu11@gmail.com>
2023-11-04 17:33:41 +1100

    Added email function

---
+++ scripts/emailJS.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+         
+    document.querySelector("#emailSendButton").onclick = () => {
+        var templateParams = {
+            name: document.querySelector('#name').value,
+            email: document.querySelector('#email').value,
+            subject: document.querySelector('#subject').value,
+            message: document.querySelector('#message').value
+        }; 
+
+        emailjs.send('service_z86y3rb', 'template_kysmiza', templateParams)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+           alert("Mail Sent! Thank you so much for reaching out.")
+        }, function(error) {
+           console.log('FAILED...', error);
+           alert("Oops! Sorry, something went wrong. Please try again.")
+        });
+
+    }
+    
+
+});
